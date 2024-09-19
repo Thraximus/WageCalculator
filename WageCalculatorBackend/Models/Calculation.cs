4e224c5d259b11f5db9bfcacbaf3704400bb37a6
Miroslav Stojanov <35580663+Thraximus@users.noreply.github.com>
2024-09-19 14:39:02 +0200

    Init .net files

---
+++ WageCalculatorBackend/Models/Calculation.cs
@@ -0,0 +1,21 @@
+﻿namespace WageCalculatorBackend.Models
+{
+    public class Calculation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public TimeSpan RegularStartTime { get; set; }
+        public TimeSpan RegularEndTime { get; set; }
+
+
+        public TimeSpan NightTimeStartTime { get; set; }
+        public TimeSpan NightTimeEndTime { get; set; }
+
+        public TimeSpan MidnightStartTime { get; set; }
+        public TimeSpan MidnightEndTime { get; set; }
+
+
+    }
+}
